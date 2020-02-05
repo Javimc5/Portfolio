0d1899b744e2bd3151d862d2135cfac426f0fbdf
Javi <javiermac1999@gmail.com>
2020-02-05 10:52:57 +0100

    Commit Final

---
+++ js/script.js
@@ -0,0 +1,19 @@
+(function($) {
+  "use strict";
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 100
+  });
+  var navbarCollapse = function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
+  navbarCollapse();
+  $(window).scroll(navbarCollapse);
+})(jQuery);
